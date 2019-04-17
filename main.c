ee3ad33d69e25535ea6fa3d7ef267cc70744bbca
wilsonrobotics2 <48923297+wilsonrobotics2@users.noreply.github.com>
2019-04-17 11:37:52 -0700

    Add files via upload
    Analog sensors and  motors are functional.

--- main.c
+++ main.c
@@ -1,38 +1,36 @@
 #include <kipr/botball.h>
 
 int main()
-{
-    printf("Hello World\n");
-    motor(0,100);//right motor
-    motor(3,100);//left motor
-    msleep(3300);//2 sec.
+{ 
+ int right_sensor = analog(3);
+ int left_sensor = analog(5);
+ int light_sensor = analog(0); 
+ printf("Must not touch the black lines");
+
+wait_for_light(light_sensor); 
     
-    motor(0,100);//right motor
-    motor(3,50);//left motor
-    msleep(2550);//3 sec.
+ motor(0,100);
+ motor(3,100);
+ msleep(1000);
     
-    motor(0,100);//right motor
-    motor(3,100);//left motor
-    msleep(1600);//3 sec.
-    
-    motor(0,50);//right motor
-    motor(3,0);//left motor
-    msleep(2600);//3 sec.
-    
-    motor(3,100);
-    motor(0,100);
-    msleep(7000);
-    motor(0,50);//right motor
-    motor(3,0);//left motor
-    msleep(2400);//3 sec.
-    motor(3,100);
-    motor(0,100);
-    msleep(1900);
-    motor(0,50);//right motor
-    motor(3,0);//left motor
-    msleep(2400);//3 sec.
-    motor(0,100);
-    motor(3,100);
-    msleep(3500);
-    return 0;
+ while ((right_sensor< 1100) && left_sensor<1100)
+ {
+     motor(0,100);
+     motor(3,100);
+     msleep(6000);
+ } 
+
+ while (analog(5) >1100)
+ {
+     motor(0,90);
+     motor(3,80);
+ }    
+
+ while (analog(3) >1100)
+ {
+     motor(0,80);
+     motor(3,90);
+ }
+ ao();
+ return 0;
 }
